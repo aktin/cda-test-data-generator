8296fa008ff53d0adc3dbffef32e2150c3eb3570
jkramer <jkramer@ukaachen.de>
2024-07-31 15:10:26 +0200

    add parser for Constraints

--- src/parser.py
+++ src/parser.py
@@ -8,7 +8,7 @@ class ValueSetParser:
         param_dict = {}
         for param in param_list:
             key, value = param.split("=")
-            if key == "range":
+            if key == "scope":
                 min_value, max_value = value.split("-")
                 param_dict["min_value"] = int(min_value) if '.' not in min_value else float(min_value)
                 param_dict["max_value"] = int(max_value) if '.' not in max_value else float(max_value)
@@ -27,5 +27,30 @@ class ValueSetParser:
                 param_dict["start_date"] = datetime.datetime.strptime(value, "%Y%m%d")
             elif key == "end_date":
                 param_dict["end_date"] = datetime.datetime.strptime(value, "%Y%m%d")
+            else:
+                raise ValueError("Input string does not match the expected format")
 
         return param_dict
+    @staticmethod
+    def parse_constraints(string_to_parse):
+        param_list = string_to_parse.split(";")
+        param_dict = {}
+        for param in param_list:
+            key, value = param.split("=")
+            if key == "min":
+                param_dict["min"] = value
+            elif key == "range":
+                pattern = r'(\d+\.?\d*)([a-zA-Z]+)'
+
+                # Use re.match to apply the pattern to the input string
+                match = re.match(pattern, string_to_parse)
+
+                if match:
+                    # Extract the value and unit from the match groups
+                    param_dict["range"] = int(match.group(1))  # Convert the numeric part to a float
+                    param_dict["unit"] = match.group(2)  # Extract the unit part as a string
+                else:
+                    raise ValueError("Input string does not match the expected format")
+
+
+
