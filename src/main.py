ddab515234868907e25f038e3b4b268ffc2ebfb4
Alexander <alexander.ivanets@rwth-aachen.de>
2024-08-05 08:09:25 +0200

    Add proper import

--- src/main.py
+++ src/main.py
@@ -1,18 +1,17 @@
-import generate_csv
-import csv_to_cda
+from generate_csv import generate_csv
+from csv_to_cda import csv_to_cda
 from calculate_dependencies import calculate_dependencies
 from src.sent_to_server_and_print_error import send_xml_file
 
 if __name__ == '__main__':
     
-    # TODO Proper import
-    data_csv = generate_csv.generate_csv('../res/CDAVariables.xlsx', 10)
+    data_csv = generate_csv('../res/CDAVariables.xlsx', 10)
 
     print()
     calculate_dependencies(data_csv)
 
     xslt_file = '../res/EmergencyNote.xslt'
-    csv_to_cda.csv_to_cda(data_csv, xslt_file)
+    csv_to_cda(data_csv, xslt_file)
     for i in range(1,11):
         send_xml_file("http://localhost:5080/aktin/cda/fhir/Binary/$validate", f"../output/cda/cda_{i}.xml",
                   f"../output/fehlercodes/response{i}.xml")
