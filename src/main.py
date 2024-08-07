fa7d22ac9eb38d76b1f3b162568bab508f25a09a
Alexander <alexander.ivanets@rwth-aachen.de>
2024-08-07 10:16:37 +0200

    Add config.toml file with path variables

--- src/main.py
+++ src/main.py
@@ -1,12 +1,24 @@
+import os
+import toml
+
 from generate_csv import generate_csv
 from csv_to_cda import csv_to_cda
 from calculate_dependencies import calculate_dependencies
 from src.sent_to_server_and_print_error import send_xml_file
 
 if __name__ == '__main__':
-    csv_path = '../resources/data.csv'
-    excel_path = '../resources/CDAVariables.xlsx'
-    xslt_file = '../resources/EmergencyNote.xslt'
+    # Load configuration from config.toml
+    config = toml.load('../config.toml')
+
+    # Set environment variables
+    os.environ['CSV_PATH'] = config['cda_paths']['csv_path']
+    os.environ['EXCEL_PATH'] = config['cda_paths']['excel_path']
+    os.environ['XSLT_FILE'] = config['cda_paths']['xslt_file']
+
+    # Get environment variables
+    csv_path = os.environ['CSV_PATH']
+    excel_path = os.environ['EXCEL_PATH']
+    xslt_file = os.environ['XSLT_FILE']
 
     # First step: Generate
     generate_csv(excel_path, csv_path, 10)
@@ -15,6 +27,10 @@ if __name__ == '__main__':
     # Third step: Transform to CDA
     csv_to_cda(csv_path, xslt_file)
 
-    for i in range(1, 11):
-        send_xml_file("http://localhost:5080/aktin/cda/fhir/Binary/$validate", f"../output/cda/cda_{i}.xml",
-                      f"../output/fehlercodes/response{i}.xml")
+    # for i in range(1, 11):
+    #     send_xml_file("http://localhost:5080/aktin/cda/fhir/Binary/$validate", f"../output/cda/cda_{i}.xml",
+    #                   f"../output/fehlercodes/response{i}.xml")
+
+    # Remove data.csv
+    os.remove(csv_path)
+
